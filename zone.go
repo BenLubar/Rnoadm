2004a5662a469cf30052f2cdb5f74c99b33196ce
Ben Lubar <ben.lubar@gmail.com>
2013-07-24 22:13:19 -0500

    better zone filename generator

--- zone.go
+++ zone.go
@@ -9,8 +9,14 @@ import (
 const root3_2 = 0.8660254037844386467637231707529361834714026269051903
 
 func zoneFilename(x, y int64) string {
-	var buf [16]byte
-	binary.LittleEndian.PutUint64(buf[:8], uint64(x))
-	binary.LittleEndian.PutUint64(buf[8:], uint64(y))
-	return base32.StdEncoding.EncodeToString(buf[:])
+	var buf [binary.MaxVarintLen64 * 2]byte
+	i := binary.PutVarint(buf[:], x)
+	i += binary.PutVarint(buf[i:], y)
+	encoded := base32.StdEncoding.EncodeToString(buf[:i])
+
+	l := len(encoded)
+	for encoded[l-1] == '=' {
+		l--
+	}
+	return "z" + encoded[:l] + ".gz"
 }
