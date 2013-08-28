45e10066365c2000e2a40a7232f5bbaea5d1a71f
Ben Lubar <ben.lubar@gmail.com>
2013-08-28 17:20:53 -0500

    workaround for https://code.google.com/p/go/issues/detail?id=6121 (go.net/websocket: chrome wss doesn't work with go-lang https/tls server)

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"code.google.com/p/go.net/websocket"
+	"crypto/tls"
 	_ "github.com/BenLubar/Rnoadm/critter"
 	"github.com/BenLubar/Rnoadm/hero"
 	_ "github.com/BenLubar/Rnoadm/material"
@@ -20,9 +21,23 @@ func main() {
 
 	http.Handle("/ws", websocket.Handler(socketHandler))
 	http.HandleFunc("/", staticHandler)
+
+	srv := &http.Server{
+		Addr: ":2064",
+		TLSConfig: &tls.Config{
+			// BUG: https://code.google.com/p/go/issues/detail?id=6121
+			CipherSuites: []uint16{
+				tls.TLS_RSA_WITH_RC4_128_SHA,
+				tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+				tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+			},
+		},
+	}
+
 	go func() {
 		for {
-			err := http.ListenAndServeTLS(":2064", "rnoadm-cert.pem", "rnoadm-key.pem", nil)
+			err := srv.ListenAndServeTLS("rnoadm-cert.pem", "rnoadm-key.pem")
 			if err != nil {
 				log.Print(err)
 				time.Sleep(time.Second)
